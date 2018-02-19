8cdc76de2ce976b51159eb24b554176a217a9451
Austin Wolfe <wolfe_austin@yahoo.com>
2018-02-19 19:54:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,6 @@ def display_board
   cell = "   "
   row = cell + "|" + cell + "|" + cell
   dash = "-----------"
-  board = row + '\n' + dash + row + '\n' + dash + row
+  board = row + "\n" + dash +"\n" + row + "\n" + dash + "\n" + row
   p board
 end 
